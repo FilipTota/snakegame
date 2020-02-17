18c942de69ed4fc41e04461ed2cf7e74215b8691
FilipTota <58394415+FilipTota@users.noreply.github.com>
2020-02-17 14:18:27 +0100

    Updates

---
+++ precache-manifest.d37341d28922cbe0c375e4086e41417b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6118be61c5c0a99ce58daa381403081",
+    "url": "/snakegame/index.html"
+  },
+  {
+    "revision": "2de7b2680d0384f5a983",
+    "url": "/snakegame/static/css/main.0d63b314.chunk.css"
+  },
+  {
+    "revision": "9bf4432deb61e402cfc7",
+    "url": "/snakegame/static/js/2.e7ecb364.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/snakegame/static/js/2.e7ecb364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de7b2680d0384f5a983",
+    "url": "/snakegame/static/js/main.ac5d0cbc.chunk.js"
+  },
+  {
+    "revision": "1141c5a1fe884f733cae",
+    "url": "/snakegame/static/js/runtime-main.ba707cf4.js"
+  }
+]);
